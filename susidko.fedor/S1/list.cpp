7ea0303c9e9f742d45d98b5096e66d36e6316743
F-psss <fpvadimovich@gmail.com>
2024-03-17 14:42:22 +0300

    add copy and move

---
+++ susidko.fedor/S1/list.cpp
@@ -0,0 +1,12 @@
+#include "list.hpp"
+#include "node.hpp"
+#include "Iterator.hpp"
+
+//template< typename T > 
+
+//template< typename T > 
+//template< typename T > 
+
+
+//template< typename T > 
+
