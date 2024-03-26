99bc2c894415794d97298a9f779369144403ac87
sadofeeeva <klover200015@gmail.com>
2024-03-26 17:25:18 +0700

    добавлениe....

--- sadofeva.nastya/S1/main.cpp
+++ sadofeva.nastya/S1/main.cpp
@@ -1,27 +1,8 @@
 #inlude <iostream>
-#include <cstring>
-#include "lish.hpp"
+#include <string>
+#include "task.hpp"
+#include "iolists.hpp"
 
 int main()
 {
-  using namespace sadofeva;
-  std::string input_str;
-  using pair = std::pair<std::string, List<int>>;
-  size number = 0;
-  while (std::cin)
-  {
-    std::cin >> input_str;
-    if (!std::cin)
-    {
-      break;
-    }
-    if (std::cin.eof())
-    {
-      break;
-    }
-    if (isdigit(input_str[0]))
-    {
-      pair.push_back(input_str[0]));
-    }
-  }
 }
