c19e6364ead91e7f35c800bb4836581eea7a70a4
pinniks <nik.nwlife@gmail.com>
2024-05-03 20:31:19 +0300

    fix

--- popov.nikita/S1/main.cpp
+++ popov.nikita/S1/main.cpp
@@ -70,7 +70,7 @@ int main()
       sum[count] += OList.iter.node->data.second.iter.node->data;
       std::cout << OList.iter.node->data.second.iter.node->data;
       OList.iter.node->data.second.iter++;
-      if (OList.iter.node->data.second.iter.node != nullptr)
+      if !(OList.iter.node->tail)
       {
         std::cout << " ";
       }
