d7bb359d79f914b85e41a34a4218f0ffc98c0350
shavel0k <anna.reznikova@icloud.com>
2024-06-01 20:08:18 +0300

    struct Node

--- reznikova.anna/S4/Tree.hpp
+++ reznikova.anna/S4/Tree.hpp
@@ -29,3 +29,25 @@ namespace reznikova
   };
 }
 
+using namespace reznikova;
+
+template< typename Key, typename Value, typename Comparator >
+class Tree< Key, Value, Comparator >::Node
+{
+public:
+  friend class Tree< Key, Value, Comparator >;
+  explicit Node(Key key_, Value value_) :
+    parent_(nullptr),
+    left_(nullptr),
+    right_(nullptr),
+    value_pair_(std::make_pair(key_, value_)),
+    height_(0)
+  {}
+private:
+  Node * parent_;
+  Node * left_;
+  Node * right_;
+  std::pair< const Key, Value > value_pair_;
+  int height_;
+};
+
