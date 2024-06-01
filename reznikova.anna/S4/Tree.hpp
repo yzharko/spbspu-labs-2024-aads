3b3e71b22279b9acfc891e460499050b0c5c5488
shavel0k <anna.reznikova@icloud.com>
2024-06-01 20:09:44 +0300

    added declaration and definition of ConstIterator

--- reznikova.anna/S4/Tree.hpp
+++ reznikova.anna/S4/Tree.hpp
@@ -51,3 +51,141 @@ private:
   int height_;
 };
 
+template< typename Key, typename Value, typename Comparator >
+class Tree< Key, Value, Comparator >::ConstIterator
+{
+  friend class Tree< Key, Value, Comparator >;
+  using this_t = ConstIterator;
+  using node_t = Node;
+  using pair_type = std::pair< const Key, Value >;
+  ConstIterator();
+  ConstIterator(node_t * node, node_t * root);
+  ConstIterator(const ConstIterator &) = default;
+  ~ConstIterator() = default;
+  this_t & operator=(const ConstIterator &) = default;
+  this_t & operator++();
+  this_t operator++(int);
+  this_t & operator--();
+  this_t operator--(int);
+  const pair_type & operator*() const;
+  const pair_type * operator->() const;
+  bool operator!=(const this_t & rhs) const;
+  bool operator==(const this_t & rhs) const;
+private:
+  node_t * node_;
+  node_t * root_;
+};
+
+template< typename Key, typename Value, typename Comparator >
+Tree< Key, Value, Comparator >::ConstIterator::ConstIterator():
+  node_(nullptr),
+  root_(nullptr)
+{}
+
+template< typename Key, typename Value, typename Comparator >
+Tree< Key, Value, Comparator >::ConstIterator::ConstIterator(node_t * node, node_t * root):
+  node_(node),
+  root_(root)
+{}
+
+template< typename Key, typename Value, typename Comparator >
+typename Tree< Key, Value, Comparator >::ConstIterator & Tree< Key, Value, Comparator >::ConstIterator::operator++()
+{
+  assert(node_ != nullptr);
+  if (node_->right_)
+  {
+    node_ = node_->right_;
+    while (node_->left_)
+    {
+      node_ = node_->left_;
+    }
+  }
+  else
+  {
+    Node * temp = node_->parent_;
+    while (temp and (node_ == temp->right_))
+    {
+      node_ = temp;
+      temp = temp->parent_;
+    }
+    node_ = temp;
+  }
+  return *this;
+}
+
+template< typename Key, typename Value, typename Comparator >
+typename Tree< Key, Value, Comparator >::ConstIterator Tree< Key, Value, Comparator >::ConstIterator::operator++(int)
+{
+  this_t result(*this);
+  ++(*this);
+  return result;
+}
+
+template< typename Key, typename Value, typename Comparator >
+typename Tree< Key, Value, Comparator >::ConstIterator & Tree< Key, Value, Comparator >::ConstIterator::operator--()
+{
+  if (node_ == nullptr)
+  {
+    node_ = root_;
+    while (node_->right)
+    {
+      node_ = node_->right;
+    }
+  }
+  else
+  {
+    if (node_->left)
+    {
+      node_ = node_->left;
+      while (node_->right)
+      {
+        node_ = node_->right;
+      }
+    }
+    else
+    {
+      Node * temp = node_->parent;
+      while (temp and (node_ == temp->left))
+      {
+        node_ = temp;
+        temp = node_->parent;
+      }
+      node_ = temp;
+    }
+  }
+  return *this;
+}
+
+template< typename Key, typename Value, typename Comparator >
+typename Tree< Key, Value, Comparator >::ConstIterator Tree< Key, Value, Comparator >::ConstIterator::operator--(int)
+{
+  this_t result(*this);
+  --(*this);
+  return result;
+}
+
+template< typename Key, typename Value, typename Comparator >
+const std::pair< const Key, Value > & Tree< Key, Value, Comparator >::ConstIterator::operator*() const
+{
+  assert(node_ != nullptr);
+  return node_->value_pair_;
+}
+
+template< typename Key, typename Value, typename Comparator >
+const std::pair< const Key, Value > * Tree< Key, Value, Comparator >::ConstIterator::operator->() const
+{
+  assert(node_ != nullptr);
+  return std::addressof(node_->value_pair_);
+}
+
+template< typename Key, typename Value, typename Comparator >
+bool Tree< Key, Value, Comparator >::ConstIterator::operator!=(const ConstIterator & rhs) const
+{
+  return !(*this == rhs);
+}
+
+template< typename Key, typename Value, typename Comparator >
+bool Tree< Key, Value, Comparator >::ConstIterator::operator==(const ConstIterator & rhs) const
+{
+  return *this == rhs;
+}
