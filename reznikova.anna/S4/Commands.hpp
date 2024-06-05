a47ca347878d63336fb7fd51e4fca7b9b01c763a
shavel0k <anna.reznikova@icloud.com>
2024-06-05 07:43:45 +0300

    some build

--- reznikova.anna/S4/Commands.hpp
+++ reznikova.anna/S4/Commands.hpp
@@ -6,7 +6,7 @@
 namespace reznikova
 {
   void outputInvalidCommand(std::ostream & out);
-  void outputError(std::ostream & out);
+  void outputEmptyError(std::ostream & out);
   std::string getValue(std::string & input, size_t & i);
   size_t getKey(std::string & input, size_t & i);
   void readFromFile(std::ifstream & is, Tree< std::string, Tree< size_t, std::string > > & treeMap);
