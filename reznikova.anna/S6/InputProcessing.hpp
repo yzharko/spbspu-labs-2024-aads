e9ac6e745bf65d62e4a68c37f6370fe93a002ec3
shavel0k <anna.reznikova@icloud.com>
2024-06-06 15:11:35 +0300

    s6 lab is near to be done

---
+++ reznikova.anna/S6/InputProcessing.hpp
@@ -0,0 +1,101 @@
+#ifndef INPUTPROCESSING_HPP
+#define INPUTPROCESSING_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <random>
+#include <forward_list>
+#include <deque>
+#include "List.hpp"
+#include "Sort.hpp"
+
+namespace reznikova
+{
+  void wrongInputOutput(std::ostream & out);
+  void sortErrorOutput(std::ostream & out);
+  template< typename T >
+  void getRandVals(size_t size, std::string type, List< T > & biList, std::forward_list< T > & fList, std::deque< T > & queue);
+  template< typename T >
+  void outputVals(std::ostream & out, T & conteiner);
+  template< typename T, typename Compare >
+  void sortData(std::ostream & out, std::string type, size_t size, Compare cmp);
+}
+
+void reznikova::wrongInputOutput(std::ostream & out)
+{
+  out << "<WRONG INPUT>\n";
+}
+
+void reznikova::sortErrorOutput(std::ostream & out)
+{
+  out << "<SORT ERROR>\n";
+}
+
+template< typename T >
+void reznikova::getRandVals(size_t size, std::string type, List< T > & biList, std::forward_list< T > & fList, std::deque< T > & queue)
+{
+  std::random_device dev;
+  std::mt19937 reng(dev());
+  for (size_t i = 0; i < size; i++)
+  {
+    if (type == "ints")
+    {
+      std::uniform_int_distribution< int > dis(-1000, 1000);
+      int value = dis(dev);
+      biList.pushBack(value);
+      fList.push_back(value);
+      queue.push_back(value);
+    }
+    else if (type == "floats")
+    {
+      std::uniform_real_distribution< float > dis(0.0f, 99.9f);
+      float value = dis(dev);
+      biList.pushBack(value);
+      fList.push_back(value);
+      queue.push_back(value);
+    }
+  }
+}
+
+template< typename T >
+void outputVals(std::ostream & out, T & conteiner)
+{
+  auto it = conteiner.begin();
+  out << *it;
+  it++;
+  while (it != conteiner.end())
+  {
+    out << " ";
+    out << *it;
+    it++;
+  }
+}
+
+template< typename T, typename Compare >
+void reznikova::sortData(std::ostream & out, std::string type, size_t size, Compare cmp)
+{
+  List< T > biList;
+  std::forward_list< T > fList;
+  std::deque< T > queue;
+  getRandVals(size, type, biList, fList, queue);
+  List< T > second_biList(biList);
+  std::deque< T > second_queue(queue);
+  std::deque< T > third_queue(queue);
+  
+  outputVals(std::cout, biList);
+  
+  qsort(biList.begin(), biList.end(), cmp);
+  shaker(second_biList.begin(), second_biList.end(), cmp);
+  qsort(fList.begin(), fList.end(), cmp);
+  qsort(queue.begin(), queue.end(), cmp);
+  shaker(second_queue.begin(), second_queue.end(), cmp);
+  std::sort(second_queue.begin(), third_queue.end(), cmp);
+  outputVals(std::cout, biList);
+  outputVals(std::cout, second_biList);
+  outputVals(std::cout, fList);
+  outputVals(std::cout, queue);
+  outputVals(std::cout, second_queue);
+  outputVals(std::cout, third_queue);
+}
+
+#endif
---
+++ reznikova.anna/S6/Sort.hpp
@@ -0,0 +1,89 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+
+#include <algorithm>
+
+namespace reznikova
+{
+  template< typename Iterator, typename Comparator >
+  void qsort(Iterator first, Iterator last, Comparator cmp);
+  template< typename Iterator, typename Comparator >
+  void shaker(Iterator first, Iterator last, Comparator cmp);
+}
+
+template< typename Iterator, typename Comparator >
+void reznikova::qsort(Iterator first, Iterator last, Comparator cmp)
+{
+  if (first == last || std::next(first) == last)
+  {
+    return;
+  }
+  auto pivot = *first;
+  std::vector<typename std::iterator_traits<Iterator>::value_type> less;
+  std::vector<typename std::iterator_traits<Iterator>::value_type> greater;
+  for (Iterator it = std::next(first); it != last; ++it) 
+  {
+    if (cmp(*it, pivot))
+    {
+      less.push_back(*it);
+    }
+    else
+    {
+      greater.push_back(*it);
+    }
+  }
+  Iterator it = first;
+  for (const auto & elem : less)
+  {
+    *it = elem;
+    ++it;
+  }
+  *it = pivot;
+  ++it;
+  for (const auto & elem : greater)
+  {
+    *it = elem;
+    ++it;
+  }
+  qsort(first, std::next(first, less.size()), cmp);
+  qsort(std::next(first, less.size() + 1), last, cmp);
+}
+
+template< typename Iterator, typename Comparator >
+void reznikova::shaker(Iterator first, Iterator last, Comparator cmp) 
+{
+  if (first == last)
+  {
+    return;
+  }
+  bool swapped = true;
+  while (swapped) 
+  {
+    swapped = false;
+    for (Iterator it = first; it != last - 1; ++it)
+    {
+      if (!cmp(*it, *(it + 1)))
+      {
+        std::iter_swap(it, it + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped)
+    {
+      break;
+    }
+    swapped = false;
+    --last;
+    for (Iterator it = last - 1; it != first; --it)
+    {
+      if (cmp(*it, *(it - 1)))
+      {
+        std::iter_swap(it, it - 1);
+        swapped = true;
+      }
+    }
+    ++first;
+  }
+}
+
+#endif
