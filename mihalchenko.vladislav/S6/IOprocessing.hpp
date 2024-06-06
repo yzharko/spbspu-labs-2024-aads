4d000690328b6d75b7f132cfe0b6b7259a72bc8a
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-06-06 15:13:18 +0300

    feat(S6): Функции заполнения deque рандомными числами и шаблонная функция вывода контейнера

---
+++ mihalchenko.vladislav/S6/IOprocessing.hpp
@@ -0,0 +1,57 @@
+#ifndef IO_PROCESSING_HPP
+#define IO_PROCESSING_HPP
+#include <iostream>
+#include <iomanip>
+#include <deque>
+#include <list>
+#include <list.hpp>
+#include <algorithm>
+// #include "sorts.hpp"
+// #include "details.hpp"
+
+namespace mihalchenko
+{
+  void fillContainerWithRandomNumbers(int size, std::deque<int> &deque, std::list<int> &bidirect_list, List<int> forward_list);
+  void fillContainerWithRandomNumbers(int size, std::deque<double> &deque, std::list<double> &bidirect_list, List<double> forward_list);
+  template <typename T>
+  void printContainer(std::ostream &, const T &container);
+}
+
+void fillRandIntsContainer(int size, std::deque<int> &deque, std::list<int> &bidirect_list, List<int> forward_list)
+{
+  for (int i = 0; i < size; i++)
+  {
+    deque.push_back(rand() % 100);
+  }
+  std::copy(deque.begin(), deque.end(), std::back_inserter(forward_list));
+  std::copy(deque.begin(), deque.end(), std::back_inserter(bidirect_list));
+}
+
+void fillRandDoublesContainer(int size, std::deque<double> &deque, std::list<double> &bidirect_list, List<double> forward_list)
+{
+  for (auto i = 0; i < size; i++)
+  {
+    deque.push_back(double(rand() % 1000) / double(rand() % 100));
+  }
+  std::copy(deque.begin(), deque.end(), std::back_inserter(forward_list));
+  std::copy(deque.begin(), deque.end(), std::back_inserter(bidirect_list));
+}
+
+template <typename T>
+void mihalchenko::printContainer(std::ostream &out, const T &container)
+{
+  for (auto iter = container.cbegin(); iter != container.cend(); iter++)
+  {
+    out << std::fixed << std::setprecision(1) << *iter;
+    if (++iter == container.cend())
+    {
+      out << "\n";
+    }
+    else
+    {
+      out << " ";
+    }
+  }
+}
+
+#endif
