d60361cac71071c22b3fd9081df6e20e49a53529
amotvei <motya2228@mail.ru>
2024-06-06 17:56:42 +0300

    fix

--- taskaev.matvei/S6/main.cpp
+++ taskaev.matvei/S6/main.cpp
@@ -23,14 +23,14 @@ int main(int argc, char **argv)
     return 1;
   }
   std::map < std::pair < std::string, std::string >, std::function< void(size_t, std::ostream&) > > cmds;
-  cmds[std::make_pair("ascending", "ints")] = workSortings< int, std::less< int > >;
-  cmds[std::make_pair("ascending", "floats")] = workSortings< float, std::less< float > >;
-  cmds[std::make_pair("descending", "ints")] = workSortings< int, std::greater< int > >;
-  cmds[std::make_pair("descending", "floats")] = workSortings< float, std::greater< float > >;
-
+  cmds[std::make_pair("ascending", "ints")] = taskaev::workSortings< int, std::less< int > >;
+  cmds[std::make_pair("ascending", "floats")] = taskaev::workSortings< float, std::less< float > >;
+  cmds[std::make_pair("descending", "ints")] = taskaev::workSortings< int, std::greater< int > >;
+  cmds[std::make_pair("descending", "floats")] = taskaev::workSortings< float, std::greater< float > >;
+  std::cout << std::fixed << std::setprecision(1);
   try
   {
-    cmds.at(std::make_pair(std::string(argv[1]),std::string(argv[2])))(size, std::cout);
+    cmds.at(std::make_pair(std::string(argv[1]), std::string(argv[2])))(size, std::cout);
   }
   catch (const std::exception& e)
   {
