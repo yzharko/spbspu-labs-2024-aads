3ac58b3fd0248bcd94dcf3356c8ec4ba9d3131d3
phohlova <hohlova.pv@edu.spbstu.ru>
2024-06-10 01:32:21 +0300

    cg

--- hohlova.polina/S4/WorkerBST.hpp
+++ hohlova.polina/S4/WorkerBST.hpp
@@ -3,22 +3,22 @@
 #include "BinarySearchTree.hpp"
 #include <vector>
 
-class WorkerBST {
-    BinarySearchTree<std::string, BinarySearchTree<int, std::string>* > bst;
-
+class WorkerBST
+{
+  BinarySearchTree<std::string, BinarySearchTree<int, std::string>* > bst;
 public:
-    WorkerBST(const std::string& path);
-    ~WorkerBST();
-    void Menu();
+  WorkerBST(const std::string& path);
+  ~WorkerBST();
+  void Menu();
 private:
-    void ReadFile(const std::string& path);
-    void Print(const std::string&);
-    void Complement(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
-    void Intersect(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
-    void Union(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
-    void ParseArguments(const std::string& str);
-    int ParseNum(const std::string& num);
-    void ReadDataSet(const std::string& dataSet);
-
+  void ReadFile(const std::string& path);
+  void Print(const std::string&);
+  void Complement(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
+  void Intersect(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
+  void Union(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end);
+  void ParseArguments(const std::string& str);
+  int ParseNum(const std::string& num);
+  void ReadDataSet(const std::string& dataSet);
 };
+
 #endif
