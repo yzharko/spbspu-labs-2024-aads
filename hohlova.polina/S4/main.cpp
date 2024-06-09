4b556abf3f9e562c9c3b272170361aa97cb2d575
phohlova <hohlova.pv@edu.spbstu.ru>
2024-06-10 01:30:10 +0300

    cg

--- hohlova.polina/S4/main.cpp
+++ hohlova.polina/S4/main.cpp
@@ -8,20 +8,20 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        std::cerr << "Error!";
-        return 1;
-    }
-    try
-    {
-        WorkerBST work(argv[1]);
-        work.Menu();
-    }
-    catch (const std::exception& err)
-    {
-        std::cout << err.what();
-        return 1;
-    }
-    return 0;
+  if (argc != 2)
+  {
+    std::cerr << "Error!";
+    return 1;
+  }
+  try
+  {
+    WorkerBST work(argv[1]);
+    work.Menu();
+  }
+  catch (const std::exception& err)
+  {
+    std::cout << err.what();
+    return 1;
+  }
+  return 0;
 }
