b0f3366584c07f60b86f96e139de4b195d057b7b
shavel0k <anna.reznikova@icloud.com>
2024-06-10 05:15:18 +0300

    some changes with inputprocessing

--- reznikova.anna/S6/InputProcessing.hpp
+++ reznikova.anna/S6/InputProcessing.hpp
@@ -34,28 +34,28 @@ void reznikova::sortErrorOutput(std::ostream & out)
 template< typename T >
 void reznikova::getRandVals(size_t size, std::string type, List< T > & biList, std::forward_list< T > & fList, std::deque< T > & queue)
 {
-  std::random_device dev;
-  std::mt19937 reng(dev());
-  for (size_t i = 0; i < size; i++)
-  {
-    if (type == "ints")
-    {
-      std::uniform_int_distribution< int > dis(-1000, 1000);
-      int value = dis(dev);
-      biList.pushBack(value);
-      fList.push_front(value);
-      queue.push_back(value);
-    }
-    else if (type == "floats")
-    {
-      std::uniform_real_distribution< float > dis(0.0f, 99.9f);
-      float value = dis(dev);
-      biList.pushBack(value);
-      fList.push_front(value);
-      queue.push_back(value);
-    }
-  }
-}
+   std::random_device rd;
+   std::mt19937 gen(rd());
+   for (size_t i = 0; i < size; ++i)
+   {
+     if (type == "ints")
+     {
+       std::uniform_int_distribution< int > dis(-10000, 10000);
+       int value = dis(gen);
+       fList.push_front(value);
+       biList.pushBack(value);
+       queue.push_back(value);
+     }
+     else if (type == "floats")
+     {
+       std::uniform_real_distribution< float > dis(0.0f, 99.9f);
+       float value = dis(gen);
+       fList.push_front(value);
+       biList.pushBack(value);
+       queue.push_back(value);
+     }
+   }
+ }
 
 template< typename T >
 void reznikova::outputVals(std::ostream & out, T & conteiner)
@@ -69,6 +69,7 @@ void reznikova::outputVals(std::ostream & out, T & conteiner)
     out << *it;
     it++;
   }
+  out << "\n";
 }
 
 template< typename T, typename Compare >
@@ -82,7 +83,7 @@ void reznikova::sortData(std::ostream & out, std::string type, size_t size, Comp
   std::deque< T > second_queue(queue);
   std::deque< T > third_queue(queue);
 
-  outputVals(out, biList);
+  outputVals(out, queue);
 
   shaker(second_biList.begin(), second_biList.end(), cmp);
   outputVals(out, second_biList);
@@ -94,7 +95,8 @@ void reznikova::sortData(std::ostream & out, std::string type, size_t size, Comp
   outputVals(out, queue);
   shaker(second_queue.begin(), second_queue.end(), cmp);
   outputVals(out, second_queue);
-  std::sort(second_queue.begin(), third_queue.end(), cmp);
+  std::sort(third_queue.begin(), third_queue.end(), cmp);
   outputVals(out, third_queue);
 }
+
 #endif
