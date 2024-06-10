7a689c201962721265b9b2dc75df75f273ec2999
amotvei <motya2228@mail.ru>
2024-06-10 16:48:29 +0300

    fix main

--- taskaev.matvei/S6/main.cpp
+++ taskaev.matvei/S6/main.cpp
@@ -14,25 +14,46 @@ int main(int argc, char **argv)
     std::cerr << "Error:\n";
     return 1;
   }
-  size_t size = std::stoul(argv[3]);
+  std::string names = argv[1];
+  if (names != "ascending" && names != "descending")
+  {
+    std::cerr << "Error:   AAA\n";
+    return 1;
+  }
+  std::string names = argv[2];
+  if (names != "ints" && names != "floats")
+  {
+    std::cerr << "Error:   typess errror\n";
+    return 1;
+  }
+  size_t size = 0;
+  try
+  {
+    size = std::stoll(argv[3]);
+  }
+  catch
+  {
+    std::cer << "eror\n";
+    return 1;
+  }
   if (size == 0)
   {
     std::cerr << "Error: size zero!\n";
     return 1;
   }
-  std::map < std::pair < std::string, std::string >, std::function< void(std::ostream&, size_t) > > cmds;
-  cmds[std::make_pair("ascending", "ints")] = taskaev::workSortings< int, std::less< int > >;
-  cmds[std::make_pair("ascending", "floats")] = taskaev::workSortings< float, std::less< float > >;
-  cmds[std::make_pair("descending", "ints")] = taskaev::workSortings< int, std::greater< int > >;
-  cmds[std::make_pair("descending", "floats")] = taskaev::workSortings< float, std::greater< float > >;
-  try
-  {
-    cmds.at(std::make_pair(std::string(argv[1]), std::string(argv[2])))(std::cout, size);
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << e.what();
-    return 1;
-  }
-  return 0;
+  //std::map < std::pair < std::string, std::string >, std::function< void(std::ostream&, size_t) > > cmds;
+  //cmds[std::make_pair("ascending", "ints")] = taskaev::workSortings< int, std::less< int > >;
+  //cmds[std::make_pair("ascending", "floats")] = taskaev::workSortings< float, std::less< float > >;
+  //cmds[std::make_pair("descending", "ints")] = taskaev::workSortings< int, std::greater< int > >;
+ // cmds[std::make_pair("descending", "floats")] = taskaev::workSortings< float, std::greater< float > >;
+ // try
+ // {
+   // cmds.at(std::make_pair(std::string(argv[1]), std::string(argv[2])))(std::cout, size);
+ // }
+ // catch (const std::exception& e)
+ // {
+   // std::cerr << e.what();
+   // return 1;
+ // }
+ // return 0;
 }
