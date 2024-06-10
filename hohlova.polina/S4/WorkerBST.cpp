f78bd3e16f5a6f2155f2c71eed4f78ecadc302d6
phohlova <hohlova.pv@edu.spbstu.ru>
2024-06-10 10:49:10 +0300

    cg

--- hohlova.polina/S4/WorkerBST.cpp
+++ hohlova.polina/S4/WorkerBST.cpp
@@ -2,332 +2,326 @@
 #include <fstream>
 #include <string>
 
-WorkerBST::WorkerBST(const std::string& path) {
-    ReadFile(path);
+WorkerBST::WorkerBST(const std::string& path)
+{
+  ReadFile(path);
 }
 
-WorkerBST::~WorkerBST() {
-    auto it = bst.begin();
+WorkerBST::~WorkerBST()
+{
+  auto it = bst.begin();
+  while (it != bst.end())
+  {
+    auto ptr = (*it).second;
+    delete  ptr;
+    ++it;
+  }
+}
 
-    while (it != bst.end())
+void WorkerBST::Menu()
+{
+  std::string line;
+  while (!std::cin.eof())
+  {
+    std::getline(std::cin, line);
+    if (!line.empty())
     {
-        auto ptr = (*it).second;
-        delete  ptr;
-        ++it;
+      ParseArguments(line);
     }
+  }
 }
+void WorkerBST::ReadFile(const std::string& path)
+{
+  if (path.empty())
+    std::runtime_error("Path is empty\n");
 
-void WorkerBST::Menu() {
+  std::ifstream file(path);
 
-    std::string line;
+  if (!file.is_open())
+    std::runtime_error("File not open\n");
 
-    while (!std::cin.eof()) {
-        std::getline(std::cin, line);
-        if (!line.empty()) {
-            ParseArguments(line);
-        }
-    }
-}
-void WorkerBST::ReadFile(const std::string& path) {
-
-    if (path.empty())
-        std::runtime_error("Path is empty\n");
-
-    std::ifstream file(path);
-
-    if (!file.is_open())
-        std::runtime_error("File not open\n");
-
-
-    std::string line;
-    while (!file.eof()) {
-        std::getline(file, line);
-        ReadDataSet(line);
-    }
-
-    file.close();
+  std::string line;
+  while (!file.eof())
+  {
+    std::getline(file, line);
+    ReadDataSet(line);
+  }
+  file.close();
 }
 
 void WorkerBST::Print(const std::string& str)
 {
-    ///   std::string word;
-    BinarySearchTree<int, std::string>* findBst = nullptr;
-    findBst = bst.get(str);
+  BinarySearchTree< int, std::string >* findBst = nullptr;
+  findBst = bst.get(str);
 
-    if (findBst == nullptr)
+  if (findBst == nullptr)
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  else if (findBst->empty())
+  {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
+  auto it = findBst->begin();
+  std::cout << str << ' ';
+  for (; it != findBst->end();)
+  {
+    std::cout << (*it).first << ' ' << (*it).second;
+    if (++it != findBst->end())
     {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
+      std::cout << ' ';
     }
-    else if (findBst->empty())
-    {
-      std::cout << "<EMPTY>\n";
-      return;
-    }
-    auto it = findBst->begin();
-    std::cout << str << ' ';
-    for (; it != findBst->end();)
-    {
-        std::cout << (*it).first << ' ' << (*it).second;
-        if (++it != findBst->end())
-        {
-            std::cout << ' ';
-        }
-    }
-    std::cout << '\n';
+  }
+  std::cout << '\n';
 }
 
-void WorkerBST::Complement(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end)
+void WorkerBST::Complement(std::vector< std::string >::iterator begin, std::vector< std::string >::iterator end)
 {
+  BinarySearchTree< int, std::string >* first = nullptr;
+  BinarySearchTree< int, std::string >* second = nullptr;
+  std::string key = *begin;
+  ++begin;
 
-    BinarySearchTree<int, std::string>* first = nullptr;
-    BinarySearchTree<int, std::string>* second = nullptr;
-
-
-    std::string key = *begin;
+  while (begin != end)
+  {
+    if (!first)
+    {
+      first = bst.get(*begin);
+    }
+    else if (!second)
+    {
+      second = bst.get(*begin);
+    }
+    if (first && second)
+      break;
     ++begin;
+  }
 
-    while (begin != end) {
+  if (!second || !first)
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
 
-        if (!first) {
-            first = bst.get(*begin);
-        }
-        else if (!second)
-        {
-            second = bst.get(*begin);
-        }
+  BinarySearchTree< int, std::string >* result = new BinarySearchTree< int, std::string >;
+  auto itF = first->begin();
+  auto itE = first->end();
 
-        if (first && second)
-            break;
-        ++begin;
-    }
-
-
-    if (!second || !first)
+  while (itF != itE)
+  {
+    auto res = second->contains((*itF).first);
+    if (!res)
     {
-      std::cout << "<INVALID COMMAND>\n";
-      return;
+      result->push((*itF).first, (*itF).second);
     }
+    ++itF;
+  }
 
-    BinarySearchTree<int, std::string>* result = new BinarySearchTree<int, std::string>;
-    auto itF = first->begin();
-    auto itE = first->end();
+  if (bst.contains(key))
+  {
+    delete bst.get(key);
+    bst.drop(key);
+  }
 
-
-    while (itF != itE)
-    {
-        auto res = second->contains((*itF).first);
-        if (!res)
-        {
-            result->push((*itF).first, (*itF).second);
-        }
-        ++itF;
-    }
-
-    if (bst.contains(key))
-    {
-        delete bst.get(key);
-        bst.drop(key);
-    }
-
-    bst.push(key, result);
+  bst.push(key, result);
 }
 
-int WorkerBST::ParseNum(const std::string& num) {
-
-    size_t pos = 0;
-    auto symbol = static_cast<unsigned char>(num[pos]);
-    long long result = 0;
-    while (std::isdigit(symbol)) {
-        result = result * 10 + (symbol - '0');
-
-        if (pos == num.size() - 1) {
-            pos++;
-            break;
-        }
-
-        symbol = static_cast<unsigned char>(num[++pos]);
-    }
-    return result;
-
-}
-
-void WorkerBST::ReadDataSet(const std::string& dataSet) {
-
-    size_t pos = 0;
-    std::string word;
-    word.reserve(10);
-    std::vector<std::string> stringList;
-    stringList.reserve(10);
-
-    while (pos <= dataSet.size()) {
-        if (std::isspace(dataSet[pos]) || pos == dataSet.size()) {
-            stringList.push_back(word);
-            word = "";
-        }
-        else {
-            word += dataSet[pos];
-        }
-        ++pos;
-    }
-
-    size_t i = 1;
-
-    BinarySearchTree<int, std::string>* newTree = new BinarySearchTree<int, std::string>;
-    bst.push(stringList[0], newTree);
-
-    while (i < stringList.size() - 1)
-    {
-        newTree->push(ParseNum(stringList[i]), stringList[i + 1]);
-        i += 2;
-    }
-}
-
-
-
-void WorkerBST::Intersect(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end)
+int WorkerBST::ParseNum(const std::string& num)
 {
-    BinarySearchTree<int, std::string>* first = nullptr;
-    BinarySearchTree<int, std::string>* second = nullptr;
-
-
-    std::string key = *begin;
-    ++begin;
-
-    while (begin != end) {
-
-        if (!first)
-            first = bst.get(*begin);
-        else if (!second)
-            second = bst.get(*begin);
-
-        ++begin;
-    }
-
-    if (!second || !first)
+  size_t pos = 0;
+  auto symbol = static_cast< unsigned char >(num[pos]);
+  long long result = 0;
+  while (std::isdigit(symbol))
+  {
+    result = result * 10 + (symbol - '0');
+    if (pos == num.size() - 1)
     {
-      std::cout << "<INVALID COMMAND>\n";
-      return;
+      pos++;
+      break;
     }
-
-    BinarySearchTree<int, std::string>* result = new BinarySearchTree<int, std::string>;
-
-    auto itF = first->begin();
-
-    while (itF != first->end())
-    {
-        auto res = second->contains((*itF).first);
-        if (res)
-        {
-            result->push((*itF).first, (*itF).second);
-        }
-        ++itF;
-    }
-
-    if (bst.contains(key))
-    {
-        delete bst.get(key);
-        bst.drop(key);
-    }
-
-    bst.push(key, result);
+    symbol = static_cast< unsigned char >(num[++pos]);
+  }
+  return result;
 }
 
-void WorkerBST::Union(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end)
+void WorkerBST::ReadDataSet(const std::string& dataSet)
 {
+  size_t pos = 0;
+  std::string word;
+  word.reserve(10);
+  std::vector< std::string > stringList;
+  stringList.reserve(10);
 
-    BinarySearchTree<int, std::string>* first = nullptr;
-    BinarySearchTree<int, std::string>* second = nullptr;
-
-
-    std::string key = *begin;
-    ++begin;
-
-    while (begin != end) {
-
-        if (!first)
-            first = bst.get(*begin);
-        else if (!second)
-            second = bst.get(*begin);
-
-        ++begin;
-    }
-
-    if (!second || !first)
+  while (pos <= dataSet.size())
+  {
+    if (std::isspace(dataSet[pos]) || pos == dataSet.size())
     {
-      std::cout << "<INVALID COMMAND>\n";
-      return;
+      stringList.push_back(word);
+      word = "";
     }
-
-    BinarySearchTree<int, std::string>* result = new BinarySearchTree<int, std::string>;
-
-    auto itF = first->begin();
-    auto itS = second->begin();
-
-    while (itF != first->end() || itS != second->end())
+    else
     {
-        if (itF != first->end())
-        {
-            if (!result->contains((*itF).first))
-            {
-                result->push((*itF).first, (*itF).second);
-            }
-            ++itF;
-        }
-
-        if (itS != second->end())
-        {
-            if (!result->contains((*itS).first))
-            {
-                result->push((*itS).first, (*itS).second);
-            }
-            ++itS;
-        }
+      word += dataSet[pos];
     }
+    ++pos;
+  }
 
-    if (bst.contains(key))
-    {
-        delete bst.get(key);
-        bst.drop(key);
-    }
+  size_t i = 1;
+  BinarySearchTree< int, std::string >* newTree = new BinarySearchTree< int, std::string >;
+  bst.push(stringList[0], newTree);
 
-    bst.push(key, result);
+  while (i < stringList.size() - 1)
+  {
+    newTree->push(ParseNum(stringList[i]), stringList[i + 1]);
+    i += 2;
+  }
 }
 
-void WorkerBST::ParseArguments(const std::string& str/*, BinarySearchTree<int, std::string> bst*/) {
+void WorkerBST::Intersect(std::vector< std::string >::iterator begin, std::vector< std::string >::iterator end)
+{
+  BinarySearchTree< int, std::string >* first = nullptr;
+  BinarySearchTree< int, std::string >* second = nullptr;
+  std::string key = *begin;
+  ++begin;
 
-    size_t pos = 0;
-    std::string word;
-    word.reserve(10);
-    std::vector<std::string> stringList;
-    stringList.reserve(10);
+  while (begin != end)
+  {
+    if (!first)
+      first = bst.get(*begin);
+    else if (!second)
+      second = bst.get(*begin);
 
-    while (pos <= str.size()) {
-        if (std::isspace(str[pos]) || pos == str.size()) {
-            stringList.push_back(word);
-            word = "";
-        }
-        else {
-            word += str[pos];
-        }
-        ++pos;
+    ++begin;
+  }
+
+  if (!second || !first)
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  BinarySearchTree< int, std::string >* result = new BinarySearchTree< int, std::string >;
+  auto itF = first->begin();
+
+  while (itF != first->end())
+  {
+    auto res = second->contains((*itF).first);
+    if (res)
+    {
+      result->push((*itF).first, (*itF).second);
+    }
+    ++itF;
+  }
+
+  if (bst.contains(key))
+  {
+    delete bst.get(key);
+    bst.drop(key);
+  }
+
+  bst.push(key, result);
+}
+
+void WorkerBST::Union(std::vector< std::string >::iterator begin, std::vector< std::string >::iterator end)
+{
+  BinarySearchTree< int, std::string >* first = nullptr;
+  BinarySearchTree< int, std::string >* second = nullptr;
+  std::string key = *begin;
+  ++begin;
+
+  while (begin != end)
+  {
+    if (!first)
+      first = bst.get(*begin);
+    else if (!second)
+      second = bst.get(*begin);
+
+    ++begin;
+  }
+
+  if (!second || !first)
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  BinarySearchTree< int, std::string >* result = new BinarySearchTree< int, std::string >;
+  auto itF = first->begin();
+  auto itS = second->begin();
+
+  while (itF != first->end() || itS != second->end())
+  {
+    if (itF != first->end())
+    {
+      if (!result->contains((*itF).first))
+      {
+        result->push((*itF).first, (*itF).second);
+      }
+      ++itF;
     }
 
-    word = stringList.front();
+    if (itS != second->end())
+    {
+      if (!result->contains((*itS).first))
+      {
+        result->push((*itS).first, (*itS).second);
+      }
+      ++itS;
+    }
+  }
 
-    if (word == "print") {
-        Print(*(stringList.begin() + 1));
+  if (bst.contains(key))
+  {
+    delete bst.get(key);
+    bst.drop(key);
+  }
+  bst.push(key, result);
+}
+
+void WorkerBST::ParseArguments(const std::string& str)
+{
+  size_t pos = 0;
+  std::string word;
+  word.reserve(10);
+  std::vector< std::string > stringList;
+  stringList.reserve(10);
+
+  while (pos <= str.size())
+  {
+    if (std::isspace(str[pos]) || pos == str.size())
+    {
+      stringList.push_back(word);
+      word = "";
     }
-    else if (word == "intersect") {
-        Intersect(stringList.begin() + 1, stringList.end());
+    else
+    {
+      word += str[pos];
     }
-    else if (word == "complement") {
-        Complement(stringList.begin() + 1, stringList.end());
-    }
-    else if (word == "union") {
-        Union(stringList.begin() + 1, stringList.end());
-    }
-    else if (word != "") {
-        std::cout << "<INVALID COMMAND>" << '\n';
-    }
+    ++pos;
+  }
+
+  word = stringList.front();
+
+  if (word == "print")
+  {
+    Print(*(stringList.begin() + 1));
+  }
+  else if (word == "intersect")
+  {
+    Intersect(stringList.begin() + 1, stringList.end());
+  }
+  else if (word == "complement")
+  {
+    Complement(stringList.begin() + 1, stringList.end());
+  }
+  else if (word == "union")
+  {
+    Union(stringList.begin() + 1, stringList.end());
+  }
+  else if (word != "")
+  {
+    std::cout << "<INVALID COMMAND>" << '\n';
+  }
 }
