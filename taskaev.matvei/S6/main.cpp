d01d313fc7136acaca9bd2ff0254eec4e5bb20f7
amotvei <motya2228@mail.ru>
2024-06-12 22:54:10 +0300

    fixed

--- taskaev.matvei/S6/main.cpp
+++ taskaev.matvei/S6/main.cpp
@@ -42,14 +42,16 @@ int main(int argc, char **argv)
     return 1;
   }
   std::map< std::string, std::map< std::string, std::function< void(std::ostream&, std::string, size_t) > > > cmds{};
-  using namespace std::placeholders;
-  cmds["ascending"]["ints"] = std::bind(workSortings< int, std::less< int > >, _1, _2 _3, std::less< int >{});
-  cmds["ascending"]["floats"] = std::bind(workSortings< float, std::less< float > >, _1, _2 _3, std::less< float >{});
-  cmds["descending"]["ints"] = std::bind(workSortings< int, std::less< int > >, _1, _2 _3, std::less< int >{});
-  cmds["desscending"]["floats"] = std::bind(workSortings< float, std::greater< float > >, _1, _2 _3, std::greater< float >{});
+  {
+    using namespace std::placeholders;
+    cmds["ascending"]["ints"] = std::bind(workSortings< int, std::less< int > >, _1, _2 _3, std::less< int >{});
+    cmds["ascending"]["floats"] = std::bind(workSortings< float, std::less< float > >, _1, _2 _3, std::less< float >{});
+    cmds["descending"]["ints"] = std::bind(workSortings< int, std::greater< int > >, _1, _2 _3, std::greater< int >{});
+    cmds["desscending"]["floats"] = std::bind(workSortings< float, std::greater< float > >, _1, _2 _3, std::greater< float >{});
+  }
   try
   {
-    cmds.at(names).at(types)(std::cout, type, size);
+    cmds.at(names).at(types)(std::cout, types, size);
   }
   catch(const std::exception&)
   {
