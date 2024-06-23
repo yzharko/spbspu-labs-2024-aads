130a56cc0c3701d048c8bd47abdf487836be2dbe
phohlova <hohlova.pv@edu.spbstu.ru>
2024-06-24 02:05:18 +0300

    whitespaces

--- hohlova.polina/S5/keySumm.hpp
+++ hohlova.polina/S5/keySumm.hpp
@@ -1,13 +1,14 @@
 #ifndef KEYSUMM_HPP
 #define KEYSUMM_HPP
 #include <string>
+#include "BinaryTreeII.hpp"
 
-int sum(int a, int b);
+int hohlova::sum(int a, int b);
 struct Key_summ
 {
   void operator()(const std::pair< const int, std::string > key_value)
   {
-    result_ = sum(result_, key_value.first);
+    result_ = hohlova::sum(result_, key_value.first);
     if (str.empty())
     {
       str += key_value.second;
