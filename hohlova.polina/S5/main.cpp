1acd364e4d22539812935be9a796770d8a260a31
phohlova <hohlova.pv@edu.spbstu.ru>
2024-06-23 23:05:26 +0300

    whitespaces

--- hohlova.polina/S5/main.cpp
+++ hohlova.polina/S5/main.cpp
@@ -6,50 +6,50 @@
 #include "BinaryTreeII.hpp"
 #include "keySumm.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
 
-    if (argc != 3)
+  try
+  {
+    BinarySearchTree<int, std::string> bst;
+    ReadFile(argv[2], bst);
+    Key_summ f;
+    Key_summ res;
+    std::string cmd = argv[1];
+    if (cmd == "ascending")
     {
-        std::cerr << "Error\n";
-        return 1;
+      res = bst.traverse_lnr(f);
     }
-
-    try
+    else if (cmd == "descending")
     {
-        BinarySearchTree<int, std::string> bst;
-        ReadFile(argv[2], bst);
-        Key_summ f;
-        Key_summ res;
-        std::string cmd = argv[1];
-        if (cmd == "ascending")
-        {
-            res = bst.traverse_lnr(f);
-        }
-        else if (cmd == "descending")
-        {
-            res = bst.traverse_rnr(f);
-        }
-        else if (cmd == "breadth")
-        {
-            res = bst.traverse_breadth(f);
-        }
-        else
-        {
-            throw std::runtime_error("Invalid command");
-        }
-        if(res.result_)
-        {
-             std::cout << res.result_;
-        }
-        if(!res.str.empty())
-        {
-            std::cout << " " << res.str << "\n";
-        }
+      res = bst.traverse_rnr(f);
     }
-    catch (const std::exception& err) {
-        std::cout << err.what();
-        return 1;
+    else if (cmd == "breadth")
+    {
+      res = bst.traverse_breadth(f);
     }
-
-    return 0;
+    else
+    {
+      throw std::runtime_error("Invalid command");
+    }
+    if(res.result_)
+    {
+      std::cout << res.result_;
+    }
+    if(!res.str.empty())
+    {
+      std::cout << " " << res.str << "\n";
+    }
+  }
+  catch (const std::exception& err)
+  {
+    std::cout << err.what();
+    return 1;
+  }
+  return 0;
 }
