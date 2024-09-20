34305487f96e33791f291e6a7a08b6a8d4c23826
leopepsi <3756035@gmail.com>
2024-09-20 22:39:24 +0300

    hhihi

--- zasulsky.egor/F0/commands.cpp
+++ zasulsky.egor/F0/commands.cpp
@@ -1,31 +1,8 @@
 #include "commands.hpp"
 #include <string>
+#include <parser.hpp>
 
-Parser::Parser(std::string toparse) :
-  parsable_(toparse),
-  current_(toparse.find_first_not_of(" "))
-{}
 
-std::string Parser::operator ()()
-{
-  if (current_ > parsable_.length())
-  {
-    return "";
-  }
-  size_t start = current_;
-  size_t end = 0;
-  auto che = parsable_.substr(current_).find_first_of(" ");
-  if (che == std::string::npos)
-  {
-    end = parsable_.length();
-  }
-  else
-  {
-    end = current_ + parsable_.substr(current_).find_first_of(" ");
-  }
-  current_ = 1 + end;
-  return parsable_.substr(start, end - start);
-}
 void help(dictOfDicts& ref, std::ostream& out)
 {
 
