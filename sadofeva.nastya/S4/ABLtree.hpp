d98e3b37bfaf01ad17fa26e4be3339da36356e1f
sadofeeeva <klover200015@gmail.com>
2024-09-21 05:50:52 +0700

    fx

--- sadofeva.nastya/S4/ABLtree.hpp
+++ sadofeva.nastya/S4/ABLtree.hpp
@@ -137,7 +137,7 @@ void BinarySearchTree<Key, Value, Compare>::Iterator::pushLeft(Node* node)
         node = node->left.get();
     }
 }
-
+ 
 template <typename Key, typename Value, typename Compare>
 typename BinarySearchTree<Key,Value,Compare>::Iterator::Iterator BinarySearchTree<Key, Value, Compare>::Iterator::operator++(int)
 {
@@ -405,8 +405,7 @@ typename BinarySearchTree<Key, Value, Compare>::ConstIterator BinarySearchTree<K
 
 // End const iterator
 template <typename Key, typename Value, typename Compare>
-typename BinarySearchTree<Key, Value, Compare>::ConstIterator
-BinarySearchTree<Key, Value, Compare>::cend() const {
+typename BinarySearchTree<Key, Value, Compare>::ConstIterator BinarySearchTree<Key, Value, Compare>::cend() const {
     return ConstIterator(nullptr);
 }
 
