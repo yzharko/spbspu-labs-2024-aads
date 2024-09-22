e0b96190e5304b604a640769bd0be7100244afd5
ilyapanov123 <g89517673116@gmail.com>
2024-09-22 23:49:01 +0300

    S616

--- panov.ilya/S6/main.cpp
+++ panov.ilya/S6/main.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
-#include <functional>
-#include <iterator>
+#include <map>
 #include <string>
-#include "functions.hpp"
-
-namespace A
-{
-  template< typename T >
-  bool less(T left, T right)
-  {
-    return *left < *right;
-  }
-  template< typename T >
-  bool greater(T left, T right)
-  {
-    return *left > *right;
-  }
-}
+#include <functional>
+#include <utility>
+#include "sortFunctions.hpp"
+#include "workSort.hpp"
 
 int main(int argc, char** argv)
 {
-  if (argc < 4)
+  using namespace panov;
+  if (argc != 4)
   {
-    std::cerr << "Error! Empty list\n";
-    return 0;
+    std::cerr << "Error: arguments!\n";
+    return 1;
+  }
+  size_t size = 0;
+  try
+  {
+    size = std::stoll(argv[3]);
+  }
+  catch (...)
+  {
+    std::cerr << "Error: invalid argument.\n";
+    return 1;
+  }
+  if (size == 0)
+  {
+    std::cerr << "Error: size zero!\n";
+    return 1;
+  }
+  std::map< std::string, std::map< std::string, std::function< void(std::ostream&, std::string, size_t) > > > cmds{};
+  {
+    using namespace std::placeholders;
+    cmds["ascending"]["ints"] = std::bind(workSortings< int, std::less< int > >, _1, _2, _3, std::less< int >{});
+    cmds["ascending"]["floats"] = std::bind(workSortings< float, std::less< float > >, _1, _2, _3, std::less< float >{});
+    cmds["descending"]["ints"] = std::bind(workSortings< int, std::greater< int > >, _1, _2, _3, std::greater< int >{});
+    cmds["descending"]["floats"] = std::bind(workSortings< float, std::greater< float > >, _1, _2, _3, std::greater< float >{});
   }
   try
   {
-    std::string cmd = argv[1];
-    if (cmd != "ascending" && cmd != "descending")
-    {
-      throw std::runtime_error("Invalid command");
-    }
-    std::string type = argv[2];
-    std::string size_str = argv[3];
-    size_t size = std::stoi(size_str);
-
-    if (type == "ints")
-    {
-      Worker< int >(cmd, size);
-    }
-    else if (type == "floats")
-    {
-      Worker< float >(cmd, size);
-    }
-    else
-    {
-      throw std::runtime_error("Invalid type");
-    }
+    cmds.at(argv[1]).at(argv[2])(std::cout, argv[2], size);
   }
-  catch (const std::exception& err)
+  catch (const std::exception&)
   {
-    std::cout << err.what();
+    std::cerr << "ERROR: invalid work programm!\n";
     return 1;
   }
   return 0;
