2d9ae18378100e9bc913389c7b406834cba40fa2
Ignitron1 <iamremotr@gmail.com>
2024-09-24 11:49:13 -0400

    Implemented list constructors and a destructor

--- smolyakov.alexander/S1/list.hpp
+++ smolyakov.alexander/S1/list.hpp
@@ -11,9 +11,29 @@ namespace smolyakov
   public:
     class Iterator;
     class ConstIterator;
+
+    List();
+    List(const List<T>& other);
+    List(List<T>&& other);
+    ~List();
+
+    T operator [] (const size_t index);
+    void pushFront(const T& value);
+    void pushBack(const T& value);
+    void popFront(const T& value);
+    void popBack(const T& value);
+    void clear();
+    void swap(size_t value1Index, size_t value2Index);
+
+    Iterator begin();
+    Iterator end();
+    ConstIterator constBegin();
+    ConstIterator constEnd();
+
   private:
-    Node<T> head_;
-    Node<T> tail_;
+    size_t size_;
+    Node<T>* head_;
+    Node<T>* tail_;
   };
 
   template<typename T>
@@ -34,5 +54,57 @@ namespace smolyakov
     ~ConstIterator() = default;
   };
 }
+
+template<typename T>
+smolyakov::List<T>::List()
+  : size_(0), head_(nullptr), tail_(nullptr) {}
+
+template<typename T>
+smolyakov::List<T>::List(const List<T>& other)
+  : size_(other.size_), head_(other.head_), tail_(other.tail_)
+{
+  if (size_ > 0)
+  {
+    head_ = new Node<T>(other.head_->value);
+    Node<T>* tmpNode = other.head_;
+    tail_ = head_;
+    while(tmpNode->next != nullptr)
+    {
+      tail_->next = new Node<T>(tmpNode->next->value);
+      tail_ = tail_->next;
+      tmpNode = tmpNode->next;
+    }
+  }
+}
+
+template<typename T>
+smolyakov::List<T>::List(List<T>&& other)
+  : size_(other.size_), head_(other.head_), tail_(other.tail_)
+{
+  other.size_ = 0;
+  other.head_ = nullptr;
+  other.tail_ = nullptr;
+}
+
+template<typename T>
+smolyakov::List<T>::~List<T>()
+{
+  clear();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
