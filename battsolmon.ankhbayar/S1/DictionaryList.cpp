7f6b1e32ea9bf830a33155866856d50bd7c19f7c
ankhbayar battsolmon <battsolmon.a20sur11a@gmail.com>
2024-10-02 00:36:08 +0300

    CG

--- battsolmon.ankhbayar/S1/DictionaryList.cpp
+++ battsolmon.ankhbayar/S1/DictionaryList.cpp
@@ -1,17 +1,14 @@
 #include "DictionaryList.h"
 #include <iostream>
-
 DictionaryList::DictionaryList()
 {
 	_head = nullptr;
 }
-
 DictionaryList::DictionaryList(DictionaryList&& other) noexcept
 {
 	_head = other._head;
 	other._head = nullptr;
 }
-
 DictionaryList::~DictionaryList()
 {
 	while (_head != nullptr)
@@ -21,19 +18,15 @@ DictionaryList::~DictionaryList()
 		_head = tmp;
 	}
 }
-
 DictionaryList& DictionaryList::operator=(DictionaryList&& other) noexcept
 {
 	_head = other._head;
 	other._head = nullptr;
-
 	return *this;
 }
-
 void DictionaryList::insert(const std::string& key, const std::string& value)
 {
 	Node* newNode = new Node(key, value);
-
 	if (_head == nullptr)
 	{
 		_head = newNode;
@@ -41,7 +34,6 @@ void DictionaryList::insert(const std::string& key, const std::string& value)
 	else
 	{
 		Node* prev = nullptr;
-
 		for (Node* node = _head; node != nullptr; node = node->next)
 		{
 			if (newNode->key == node->key)
@@ -52,7 +44,6 @@ void DictionaryList::insert(const std::string& key, const std::string& value)
 			else if (newNode->key < node->key)
 			{
 				newNode->next = node;
-
 				if (prev == nullptr)
 				{
 					_head = newNode;
@@ -68,26 +59,21 @@ void DictionaryList::insert(const std::string& key, const std::string& value)
 				node->next = newNode;
 				break;
 			}
-
 			prev = node;
 		}
 	}
 }
-
 void DictionaryList::remove(const std::string& key, bool deleteWordsMethod)
 {
 	Node* prev = nullptr;
 	bool flag = false;
-
-	Node* toDelete = nullptr;
-
+    Node* toDelete = nullptr;
 	for (Node* node = _head; node != nullptr; node = node->next)
 	{
 		if (node->key == key)
 		{
 			flag = true;
 			toDelete = node;
-
 			if (prev == nullptr)
 			{
 				_head = node->next;
@@ -103,10 +89,8 @@ void DictionaryList::remove(const std::string& key, bool deleteWordsMethod)
 			node->next = nullptr;
 			break;
 		}
-
 		prev = node;
 	}
-
 	if ((!flag) && (!deleteWordsMethod))
 	{
 		std::cerr << "The word which you want to delete is not in the dictionary anyway!" << std::endl;
@@ -116,7 +100,6 @@ void DictionaryList::remove(const std::string& key, bool deleteWordsMethod)
 		delete toDelete;
 	}
 }
-
 bool DictionaryList::search(const std::string& key) const
 {
 	for (Node* node = _head; node != nullptr; node = node->next)
@@ -128,35 +111,28 @@ bool DictionaryList::search(const std::string& key) const
 	}
 	return false;
 }
-
 void DictionaryList::merge(DictionaryList& other)
 {
 	while (other._head != nullptr)
 	{
 		this->insert(other._head->key, other._head->value);
-
 		Node* tmp = other._head;
 		other._head = other._head->next;
 		delete tmp;
 	}
 }
-
 void DictionaryList::deleteWords(const DictionaryList& other)
 {
 	Node* current = other._head;
-
 	while (current != nullptr)
 	{
 		this->remove(current->key, true);
-
 		current = current->next;
 	}
 }
-
 void getIntersection(const DictionaryList& first, const DictionaryList& second, DictionaryList& res)
 {
 	Node* prev = nullptr;
-
 	for (Node* node = second._head; node != nullptr; node = node->next)
 	{
 		if (first.search(node->key))
@@ -174,11 +150,9 @@ void getIntersection(const DictionaryList& first, const DictionaryList& second, 
 		}
 	}
 }
-
 void DictionaryList::printContent() const
 {
 	std::cout << "Dictionary content in the {<key> : <value>} format:" << std::endl;
-
 	for (Node* node = _head; node != nullptr; node = node->next)
 	{
 		std::cout << node->key << " : " << node->value << std::endl;
