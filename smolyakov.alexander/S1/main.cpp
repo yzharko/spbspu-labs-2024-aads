344d14a8da8be158d695b66fb4b1f917439ffe83
Ignitron1 <iamremotr@gmail.com>
2024-10-02 13:22:13 -0400

    Added overflow error handling

--- smolyakov.alexander/S1/main.cpp
+++ smolyakov.alexander/S1/main.cpp
@@ -5,36 +5,37 @@
 
 int main()
 {
-  smolyakov::List<std::pair<std::string, smolyakov::List<size_t>>> inputData = smolyakov::inputList(std::cin);
   smolyakov::List<size_t> sums;
   smolyakov::List<smolyakov::List<size_t>> rearrangedLists;
 
-  if (inputData.isEmpty())
-  {
-    std::cout << "0\n";
-    return 0;
-  }
-
-  smolyakov::outputPairListNames(std::cout, inputData);
-  
-  smolyakov::rearrangeValues(inputData, rearrangedLists);
-  if (rearrangedLists.isEmpty())
-  {
-    std::cout << "0\n";
-    return 0;
-  }
-
   try
   {
+    smolyakov::List<std::pair<std::string, smolyakov::List<size_t>>> inputData = smolyakov::inputList(std::cin);
+
+    if (inputData.isEmpty())
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+
+    smolyakov::outputPairListNames(std::cout, inputData);
+  
+    smolyakov::rearrangeValues(inputData, rearrangedLists);
+    if (rearrangedLists.isEmpty())
+    {
+      std::cout << "0\n";
+     return 0;
+    } 
+
     smolyakov::calculateSums(rearrangedLists, sums);
+
+    smolyakov::outputRearrangedLists(std::cout, rearrangedLists);
+    smolyakov::outputListValues(std::cout, sums);
+    return 0;
   }
   catch (std::overflow_error& e)
   {
-    std::cout << e.what();
+    std::cerr << e.what() << '\n';
     return 1;
   }
-
-  smolyakov::outputRearrangedLists(std::cout, rearrangedLists);
-  smolyakov::outputListValues(std::cout, sums);
-  return 0;
 }
