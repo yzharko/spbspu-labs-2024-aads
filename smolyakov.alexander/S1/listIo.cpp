d9bce484e3bfccd41e152bfc42b4f129ba4b1ba6
Ignitron1 <iamremotr@gmail.com>
2024-10-02 14:33:48 -0400

    Changed error message trying to fix acceptance failed tests

--- smolyakov.alexander/S1/listIo.cpp
+++ smolyakov.alexander/S1/listIo.cpp
@@ -29,7 +29,7 @@ listOfPairs smolyakov::inputList(std::istream& inputStream)
       }
       catch (std::out_of_range& e)
       {
-        throw std::overflow_error("Could not read input list: value too big");
+        throw std::overflow_error("overflow error");
       }
       list.end()->second.pushBack(inputNumber);
     }
