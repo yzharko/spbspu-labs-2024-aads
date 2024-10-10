e0f2aa61ce45cd44c159e83ba6f98f6f0b8e83bd
ankhbayar battsolmon <147667384+tsomossi@users.noreply.github.com>
2024-10-11 00:19:50 +0300

    CG

--- battsolmon.ankhbayar/S1/DictionaryList.h
+++ battsolmon.ankhbayar/S1/DictionaryList.h
@@ -4,34 +4,34 @@
 
 struct Node  // If Node is inside DictionaryList, the friend fuction doesn't see Node
 {
-	std::string key;
-	std::string value;
-	Node* next;
+  std::string key;
+  std::string value;
+  Node* next;
 
-	Node() : next(nullptr) {}
-	Node(const std::string& key, const std::string& value = "") : key(key), value(value), next(nullptr) {}
+  Node() : next(nullptr) {}
+  Node(const std::string& key, const std::string& value = "") : key(key), value(value), next(nullptr) {}
 };
 
 class DictionaryList
 {
-	Node* _head;
+  Node* _head;
 
 public:
-	DictionaryList();  // Rule of Five
-	DictionaryList(const DictionaryList& other) = delete;
-	DictionaryList(DictionaryList&& other) noexcept;
-	~DictionaryList();
+  DictionaryList();  // Rule of Five
+  DictionaryList(const DictionaryList& other) = delete;
+  DictionaryList(DictionaryList&& other) noexcept;
+  ~DictionaryList();
 
-	DictionaryList& operator=(const DictionaryList& other) = delete;
-	DictionaryList& operator=(DictionaryList&& other) noexcept;
+  DictionaryList& operator=(const DictionaryList& other) = delete;
+  DictionaryList& operator=(DictionaryList&& other) noexcept;
 
-	void insert(const std::string& key, const std::string& value = "");
-	void remove(const std::string& key, bool deleteWordsMethod = false);
-	bool search(const std::string& key) const;
+  void insert(const std::string& key, const std::string& value = "");
+  void remove(const std::string& key, bool deleteWordsMethod = false);
+  bool search(const std::string& key) const;
 
-	void merge(DictionaryList& other);
-	void deleteWords(const DictionaryList& other);
-	friend void getIntersection(const DictionaryList& first, const DictionaryList& second, DictionaryList& res);
+  void merge(DictionaryList& other);
+  void deleteWords(const DictionaryList& other);
+  friend void getIntersection(const DictionaryList& first, const DictionaryList& second, DictionaryList& res);
 
-	void printContent() const;
+  void printContent() const;
 };
